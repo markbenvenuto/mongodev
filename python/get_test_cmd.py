63ee7adca2ec4c1e305ca21b37e8b6c0ff0620da
Mark Benvenuto <mark.benvenuto@mongodb.com>
2021-02-01 19:59:56 -0500

    Add enterprise module support

--- python/get_test_cmd.py
+++ python/get_test_cmd.py
@@ -2,6 +2,18 @@ import argparse
 import os.path
 import sys
 
+def camel_to_snake(suite):
+    # Convert from camel case to snake case
+    orig = suite
+    suite = ''
+    for x in orig:
+        if x.isupper():
+            suite += '_' + x.lower()
+        else:
+            suite += x
+
+    return suite
+
 def get_suite(file):
 
     if file.startswith("jstests"):
@@ -13,14 +25,18 @@ def get_suite(file):
         # Replica sets needs to be expanded out
         suite = suite.replace("replsets", "replica_sets")
 
-        # Convert from camel case to snake case
-        orig = suite
-        suite = ''
-        for x in orig:
-            if x.isupper():
-                suite += '_' + x.lower()
-            else:
-                suite += x
+        camel_to_snake(suite)
+
+        return suite
+
+    # Handle enterprise
+    if file.startswith("src"):
+        parts = file.split("/")
+
+        assert len(parts) == 8
+        suite = parts[6]
+
+        camel_to_snake(suite)
 
         return suite
 
@@ -28,4 +44,4 @@ def get_suite(file):
 if __name__ == "__main__":
 
     relative_test_file = sys.argv[1]
-    print("run --suite=%s %s" %(get_suite(relative_test_file), relative_test_file))
+    print("--suite=%s %s" %(get_suite(relative_test_file), relative_test_file))
